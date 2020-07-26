e34d0fabc998dd81878bafb2535aa4c3cced70dc
sidemt <25644062+sidemt@users.noreply.github.com>
2020-07-26 13:42:06 +0900

    Upload to Glitch

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,41 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+// var MongoClient = require('mongodb');
+const https = require('https');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+// const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      const options = {
+        hostname: 'repeated-alpaca.glitch.me',
+        path: '/v1/stock/GOOG/quote',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'accept': 'application/json'
+        }
+      }
+
+      const https_req = https.request(options, (res2) => {
+        let data = '';
+        // A chunk of data has been recieved.
+        res2.on('data', (d) => {
+          data += d;
+        });
+        // The whole response has been received. Print out the result.
+        res2.on('end', () => {
+          console.log(data);
+          res.json(data);
+        })
+      }).on('error', (e) => {
+        res.send('Error on accessing external API.');
+      })
+      https_req.end();
+
     });
-    
+
 };
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
+var helmet = require('helmet');
+
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +19,14 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"]
+  }
+} ));
 
 //Index page (static HTML)
 app.route('/')
@@ -27,9 +37,9 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
